6d471cb83a2bde41c265d8b70f51a42132c6e2d3
bhavyagupta1 <48151042+bhavyagupta1@users.noreply.github.com>
2020-11-07 19:11:14 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,6 +2,9 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/App";
 
-
-
+render() {
+return {
+<p>I am learning React. My life is getting better.</p>
+}
+ }
 ReactDOM.render(<App />, document.getElementById("root"));
